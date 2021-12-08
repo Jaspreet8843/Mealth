287133df2f8f017c5e87e9ae67e1dfaa2a0560cb
JASPREET SINGH <38997592+Jaspreet8843@users.noreply.github.com>
2021-12-08 19:15:34 +0530

    bugfixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,26 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-150px";
+    var currentScrollPos = window.pageYOffset;
+    if(currentScrollPos>50)
+    {
+      /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-150px";
+      }
   }
   prevScrollpos = currentScrollPos;
+}
+
+
+/* Check if element is visible in viewport */
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
